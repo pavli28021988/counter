051c88eeca71196e7bfced348d0ed8ab5cbe9c7f
Pavlina Ivanova <pavlina33331988@gmail.com>
2024-01-26 16:15:20 +0200

    added
    counter with js

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿
+    let count = 0;
+    const buttons = document.querySelectorAll('.containerBtn');
+
+    buttons.forEach((el,index)=>{
+        el.addEventListener('click', ()=>{
+           
+            if(el.classList.contains('prevBtn')){
+                count--
+
+            }else if(el.classList.contains('nextBtn')){
+                count++
+
+            }
+
+            const counter = document.getElementById('counter');
+            counter.textContent = count;
+
+            if(count< 0){
+                counter.style.color = 'red'
+            }else if(count > 0){
+                counter.style.color = 'green'
+            }else{
+                counter.style.color = '#333333'
+            }
+
+            
+        })
+    })
